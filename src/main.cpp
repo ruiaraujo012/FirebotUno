0b6e9b572bec4d336af93174764f8b26ca67d215
ruiaraujo012 <ruiaraujo012@gmail.com>
2020-06-23 00:10:47 +0100

    :sparkles: Sonar measurement added

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,58 @@
 /*
   FirebotUno
   Author: Rui Araújo
-  Version: 0.1.0
+  Version: 0.2.0
 */
 #include <Arduino.h>
 #include <Servo.h>
-#include <TimerOne.h>
+#include <MsTimer2.h>
 
 /** 
  * Motors pin definition
  */
-#define enableMotorRight 5 // ENB (PWM)
-#define frontMotorRight 8  // IN4
-#define backMotorRight 7   // IN3
+#define enableMotorRight 6 // ENB (PWM)
+#define frontMotorRight 11 // IN4
+#define backMotorRight 10  // IN3
 
-#define enableMotorLeft 3 // ENA (PWM)
-#define frontMotorLeft 4  // IN2
-#define backMotorLeft 2   // IN1
+#define enableMotorLeft 5 // ENA (PWM)
+#define frontMotorLeft 8  // IN2
+#define backMotorLeft 7   // IN1
 
 /**
- * Servo for Sonar and Sonar
+ * Servo and sonar
  */
-#define triggerSonar 9
-#define ecoSonar 10
+#define triggerSonar 4
+#define echoSonar 3 // Interrupt 1 (External)
+
 Servo servoSonar;
+
 volatile bool state;
-byte trigger;
-byte counter;
+volatile byte trigger;
+volatile byte counter;
+unsigned long echoMeasurement[3]; // Right, Front, Left
+volatile byte echoMeasurementIndex;
 
 /**
  * Auxiliar variables
  */
 unsigned long prevMillis;
-unsigned int periodTime = 10000;
-byte count = 0;
+unsigned int periodTime = 100;
 
 /**
  * Instantiate functions
  */
 void move(int motorLeft, int motorRight);
 void brake(byte motorR, byte motorL);
-void servoSonarMove();
+void sonarTrigger();
+void echoSonarMeasurement();
 
+// ******************************************
+// *** Setup
+// ******************************************
 void setup()
 {
   Serial.begin(9600);
+
   /**
    * Motors
    */
@@ -56,35 +64,51 @@ void setup()
   pinMode(backMotorLeft, OUTPUT);
 
   /**
-   * Servo and Sonar
+   * Servo and sonar
    */
+  // Sonar pins
   pinMode(triggerSonar, OUTPUT);
-  pinMode(ecoSonar, INPUT);
+  pinMode(echoSonar, INPUT);
 
-  Timer1.initialize(5000000);
-  Timer1.attachInterrupt(triggerSonar);
-  interrupts();
+  // Timer for sonar trigger
+  MsTimer2::set(250, sonarTrigger); // 250ms period
+  MsTimer2::start();
 
-  servoSonar.attach(6);
-  servoSonar.write(0);
+  // Echo interrupt
+  attachInterrupt(digitalPinToInterrupt(echoSonar), echoSonarMeasurement, CHANGE);
 
+  // Servo
+  servoSonar.attach(9);
+  servoSonar.write(10);
+
+  // Other variables
   state = false;
   trigger = HIGH;
   counter = 0;
+  echoMeasurementIndex = 0;
 
-  //prevMillis = millis();
+  prevMillis = millis();
 }
 
+// ******************************************
+// *** Loop
+// ******************************************
 void loop()
 {
 
-  // Serial.print("")
+  if (millis() - prevMillis >= periodTime)
+  {
+    Serial.print("Couter: ");
+    Serial.println(counter);
 
-  // if (millis() - prevMillis >= periodTime)
-  // {
-  //   servoSonarMove();
-  //   prevMillis = millis();
-  // }
+    Serial.print("Echo R: ");
+    Serial.println(echoMeasurement[0]);
+
+    Serial.print("Echo F: ");
+    Serial.println(echoMeasurement[1]);
+
+    prevMillis = millis();
+  }
 
   // move(150, 150);
 }
@@ -92,6 +116,8 @@ void loop()
 /**
  *  Functions
  */
+
+// Move robot wheels
 void move(int motorLeft, int motorRight)
 {
   motorRight = constrain(motorRight, -255, 255);
@@ -103,12 +129,12 @@ void move(int motorLeft, int motorRight)
     digitalWrite(backMotorRight, HIGH);
     analogWrite(enableMotorRight, -motorRight);
   }
-  // else if (motorRight == 0)
-  // {
-  //   digitalWrite(frontMotorRight, LOW);
-  //   digitalWrite(backMotorRight, LOW);
-  //   analogWrite(enableMotorRight, motorRight);
-  // }
+  else if (motorRight == 0)
+  {
+    digitalWrite(frontMotorRight, LOW);
+    digitalWrite(backMotorRight, LOW);
+    analogWrite(enableMotorRight, motorRight);
+  }
   else
   {
     digitalWrite(frontMotorRight, HIGH);
@@ -122,12 +148,12 @@ void move(int motorLeft, int motorRight)
     digitalWrite(backMotorLeft, HIGH);
     analogWrite(enableMotorLeft, -motorLeft);
   }
-  // else if (motorLeft == 0)
-  // {
-  //   digitalWrite(frontMotorLeft, LOW);
-  //   digitalWrite(backMotorLeft, LOW);
-  //   analogWrite(enableMotorLeft, motorLeft);
-  // }
+  else if (motorLeft == 0)
+  {
+    digitalWrite(frontMotorLeft, LOW);
+    digitalWrite(backMotorLeft, LOW);
+    analogWrite(enableMotorLeft, motorLeft);
+  }
   else
   {
     digitalWrite(frontMotorLeft, HIGH);
@@ -136,6 +162,7 @@ void move(int motorLeft, int motorRight)
   }
 }
 
+// Brake robot
 void brake(byte motorR, byte motorL)
 {
 
@@ -154,29 +181,51 @@ void brake(byte motorR, byte motorL)
   }
 }
 
-void servoSonarMove()
+// Trigger the sonar and move servo attached to him
+// Timer interrupt function
+void sonarTrigger()
 {
-  Serial.println("Aqui");
-  switch (count)
+  /**
+   * We will change later when we have state machine working.
+   * If the state is (Follow Wall Right) only rotate servo to 0 -> 90 -> 0.
+   * No need to search for left wall.
+   * Right now, it stay 4 times on right and 2 times on front (repeatedly)
+   */
+  switch (counter)
   {
   case 0:
-    servoSonar.write(10);
-    count = 1;
+    servoSonar.write(10);     // Rotate servo to the right
+    echoMeasurementIndex = 0; // Index to save measured echo
+    digitalWrite(triggerSonar, trigger);
+    trigger = !trigger;
+    counter++;
     break;
 
   case 1:
-    servoSonar.write(90);
-    count = 2;
+  case 3:
+    digitalWrite(triggerSonar, trigger);
+    trigger = !trigger;
+    counter++;
     break;
 
   case 2:
-    servoSonar.write(180);
-    count = 3;
+    digitalWrite(triggerSonar, trigger);
+    trigger = !trigger;
+    counter++;
     break;
 
-  case 3:
-    servoSonar.write(90);
-    count = 0;
+  case 4:
+    servoSonar.write(90);     // Rotate servo to the front
+    echoMeasurementIndex = 1; // Index to save measured echo
+    digitalWrite(triggerSonar, trigger);
+    trigger = !trigger;
+    counter++;
+    break;
+
+  case 5:
+    digitalWrite(triggerSonar, trigger);
+    trigger = !trigger;
+    counter = 0;
     break;
 
   default:
@@ -184,41 +233,18 @@ void servoSonarMove()
   }
 }
 
-void sonarTrigger()
+// External interrupt function to measure sonar distance
+void echoSonarMeasurement()
 {
-  /**
-   * We will change later when we have state machine working.
-   * If the state is (Follow Wall Right) only rotate servo to 0 -> 90 -> 0.
-   * No need to search for left wall.
-   */
-  switch (counter)
+  static unsigned long echo;
+  unsigned int echoPinState = digitalRead(echoSonar);
+
+  if (echoPinState)
   {
-  case 0:
-    digitalWrite(triggerSonar, trigger);
-    trigger = !trigger;
-    counter++;
-    break;
-
-  case 1:
-    digitalWrite(triggerSonar, trigger);
-    trigger = !trigger;
-    counter++;
-    servoSonar.write(90);
-    break;
-
-  case 2:
-    digitalWrite(triggerSonar, trigger);
-    trigger = !trigger;
-    counter++;
-    break;
-
-  case 3:
-    digitalWrite(triggerSonar, trigger);
-    trigger = !trigger;
-    counter++;
-    servoSonar.write(0);
-    counter = 0;
-    state = true;
-    break;
+    echo = micros();
+  }
+  else
+  {
+    echoMeasurement[echoMeasurementIndex] = ((micros() - echo) >> 5); // Shift data to have a more small number
   }
 }
